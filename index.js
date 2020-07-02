a8b88ca535acafcf26d2ddd8bf158664c0550a35
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-02 22:21:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function takeANumber(katzDeliLine, name) {
     return `Welcome, ${name}. You are number ${lineNumber} in line.`
 
 };
+
+
+//2.nowServing
+
+function nowServing() {
+  if (katzDeliLine.length = 0) {
+    return `There is noboby waiting to be served!`
+  } else {
+    return `Currently serving ${katzDeliLine[0]}`
+  }
+
+  
+};
