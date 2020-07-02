743408dba4dfd2dedb32b79c74736d985151d793
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-02 22:34:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) {
-    return `Currently serving ${katzDeliLine.shift()}
+    return `Currently serving ${katzDeliLine.shift()}``
   } else {
-    return `There is nobody waiting to be served!`
+    return There is nobody waiting to be served!`
   }
 
 
