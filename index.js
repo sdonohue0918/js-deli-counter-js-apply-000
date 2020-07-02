ef47cbb47a2778cdedb4b8850d62de0cea89aac8
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-02 22:32:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine[0]}.`
   } else {
-    return `The line is currently empty.`
+    return `There is nobody waiting to be served!`
   }
 
 
