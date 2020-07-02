1cedcc669fe47972480c9ae86b3f4c50c0dbaf4b
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-02 22:15:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var katzDeliLine = []
 function takeANumber(katzDeliLine, name) {
     var lineNumber = katzDeliLine.length
     katzDeliLine.push(name)
-        
-    
+
+
     return `Welcome, ${name}. You are number ${lineNumber} in line.`
 
 };
